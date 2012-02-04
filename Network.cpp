09c921c8c34f4e18fad129ce7b66b877502b43ad
Franky Cheung <franky.cheung456@gmail.com>
2012-02-03 18:11:39 -0700

    Finished network and network.h sorta...may have bugs since long time i did classes

--- Network.cpp
+++ Network.cpp
@@ -1,3 +1,57 @@
 /***********************************
  * Implementation of Network.h
  ***********************************/
+ 
+ #include "Network.h"
+ #include <iostream"
+ 
+ Network::Network()
+ {
+	//Initialization of server
+	memset(&server,0,sizeof(server));
+	server.sin_family = AF_INET;
+	//PORT 6130 from Header
+	server.sin_port = htons(PORT);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	//obtaining server host ip.
+	host = gethostbyname("pason1.enel.ucalgary.ca");
+	
+	if( host == NULL)
+	{
+		fprintf(stderr,"could not find host: pason1.enel.ucalgary.ca");
+		exit(1);
+	}
+	//Setting host.
+	bcopy(host->h_addr, &server.sin_addr, host->h_length);
+ }
+ 
+ void Network::connect()
+ {
+	if( (serverSockfd = socket(PF_INET, SOCK_STREAM,0) ) == -1 )
+	{
+		fprintf(stderr,"Failed to create client socket socket");
+		exit(1);
+	}	
+
+	
+	if( connect(serverSockfd, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) == -1)
+	{
+		fprintf(stderr,"failed to connect to server");
+		exit(1);
+	}
+	
+}
+
+
+char * Network::receiveMessage()
+{
+	bytes = recv(serverSockfd, messageBack, MAX_MESSAGE_LENGTH, 0);
+	if(bytes > 0)
+		return messageBack();
+	return NULL;
+}
+
+void Network::sendMessage(char* messageToSend)
+{
+	send(serverSockfd,messageToSend,strlen(messageToSend),0);
+}
--- Network.h
+++ Network.h
@@ -3,12 +3,32 @@
  * reconnects, data sending and data
  * retrieval.
  ***********************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <inttypes.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h> 
 
+#define MAX_MESSAGE_LENGTH 10000
+#define PORT 6130
 class Network
 {
 public:
     // Function prototypes here
+	void connect();
+	char* receiveMessage();
+	void sendMessage(char*);
     
 private:
     // Function prototypes here
+	int serverSockfd;
+	struct sockaddr_in server;
+	struct hostent *host;
+	char messageBack[MAX_MESSAGE_LENGTH];
+	int bytes;	
 };
