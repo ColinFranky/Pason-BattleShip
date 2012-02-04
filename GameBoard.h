0ff0c80262b36eb1f01a5cf8cc494e2361882225
Colin Williams <colin@colincwilliams.com>
2012-02-03 19:22:14 -0700

    Added GameBoard class.

---
+++ GameBoard.h
@@ -0,0 +1,26 @@
+#ifndef GAME_H
+
+#define GAME_H
+
+/***********************************
+ * Represents the 3D game board,
+ * tracking what segments have been
+ * hit, missed or not shot at.
+ ***********************************/
+
+enum status_t {UNKNOWN, HIT, MISS};
+
+class GameBoard
+{
+public:
+    GameBoard( x, y, z );
+    
+    status_t getStatus( x, y, z );
+    
+    void setStatus( x, y, z, status_t status );
+    
+private:
+    int*** board;
+}
+
+#endif
